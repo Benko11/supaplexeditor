97f3ad5992aea92b7b74f63553e8f17ab2ed588e
Benjamin Bergstrom <benko.dubovecky@gmail.com>
2018-02-21 19:34:43 +0100

    Very small refactor

--- src/field.php
+++ src/field.php
@@ -6,10 +6,11 @@ foreach ($structure as $key=>$element) {
     if ($key >= 1529) break;
     $field = $elements[$element];
 
+    // format element names to file names
     $fileName = strtolower($field);
     $fileName = str_replace(' ', '', $fileName);
 
-    echo '<td style="background: url('. BASE_PATH .'/icons/'.$fileName.'.png);width:32px;height:32px;display:inline-block" data-position="el-'.$i.'">'.'</td>';
+    echo '<td style="background: url('. BASE_PATH .'/icons/'.$fileName.'.png);" data-position="el-'.$i.'">'.'</td>';
 
     $i++;
     echo ( ($i) % 60 == 0) ? '</tr><tr>' : '';
