1512c68cad58127d4e1c9fdd84b7b86006c5decd
Hectix <Kich159@icloud.com>
2018-02-26 16:21:32 +0100

    Mouse dragging - IMPLEMENTED

--- src/js/editor.js
+++ src/js/editor.js
@@ -5,13 +5,23 @@ document.addEventListener('contextmenu', function(e) {
 const fields = document.querySelectorAll('table#field td');
 const elements = document.querySelector('#elements');
 let activeElement = elements[0].value;
+let activeClick;
 
 fields.forEach(field => field.addEventListener('mousedown', function(e){
     if (e.which === 1) {
         editObj.changeElement(field);
     }
+    activeClick = 1;
+}));
 
-    // TODO: implement mouse dragging
+fields.forEach(field => field.addEventListener('mouseup', function(){
+    activeClick = 0;
+}));
+
+fields.forEach(field => field.addEventListener('mousemove', function(e){
+    if (e.which === 1 && activeClick === 1) {
+        editObj.changeElement(field);
+    }
 }));
 
 elements.addEventListener('change', editObj.changeActive);
