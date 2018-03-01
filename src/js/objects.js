9965a48a5560f0f3cb4429d3f6bb423499edb4d6
Hectix <Kich159@icloud.com>
2018-03-01 17:19:17 +0100

    Element Count - ADD

--- src/js/objects.js
+++ src/js/objects.js
@@ -1,8 +1,13 @@
 let editObj = {
     changeElement: function(field) {
         field.style.background = `url(src/icons/${activeElement}.png)`;
+        field.dataset.type = activeElement;
     },
     changeActive: function() {
         activeElement = this.value;
+    },
+    countElements: function (elementName) {
+        let count = document.querySelectorAll("td[data-type="+elementName);
+        return count.length - 1;
     }
 };
