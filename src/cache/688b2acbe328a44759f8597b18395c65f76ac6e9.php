b01f2fc68e1ca18982172851c3469f44f963d3ec
Benjamin Bergstrom <benko.dubovecky@gmail.com>
2019-01-10 19:34:30 +0100

    ADD cache

--- src/cache/688b2acbe328a44759f8597b18395c65f76ac6e9.php
+++
@@ -1,9 +0,0 @@
-<?php $__env->startSection('content'); ?>
-    <section class="level">
-        <?php echo $level; ?>
-
-    </section>
-
-    <?php echo $__env->make('field.mainMenu', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('field.master', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
