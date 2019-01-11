5582e2e1d84dc9eaefbb4e1f45214c321ff27756
Benjamin Bergstrom <benko.dubovecky@gmail.com>
2019-01-11 15:56:30 +0100

    ADD Bootstrap as an NPM package

--- src/resources/views/field/master.blade.php
+++ src/resources/views/field/master.blade.php
@@ -4,8 +4,7 @@
     <title>Supaplex Editor</title>
     <meta charset="utf-8">
 
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/app.css">
+	<link rel="stylesheet" href="css/app.css">
 </head>
 <body>
 
