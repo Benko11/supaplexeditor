d90a22f44ffd13319a990846c939fff332b3965d
Benjamin Bergstrom <benko.dubovecky@gmail.com>
2019-01-15 20:13:44 +0100

    ADD elements to the API

---
+++ api/app/Http/Controllers/ElementController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Field\FieldRender;
+use Illuminate\Support\Facades\Storage;
+use App\File;
+
+class ElementController extends Controller {
+    public function index() {
+        return response()->json(['data' => elements()]);
+    }
+
+    public function indexImages() {
+        return response()->json(['data' => elementsImages()]);
+    }
+}
--- api/routes/web.php
+++ api/routes/web.php
@@ -2,6 +2,8 @@
 
 $router->group(['prefix' => 'api/v1'], function() use ($router){
     $router->get('levels/{id}', 'LevelController@show');
+    $router->get('elements', 'ElementController@index');
+    $router->get('elements/images', 'ElementController@indexImages');
 });
 
 $router->get('/test', 'LevelController@test');
