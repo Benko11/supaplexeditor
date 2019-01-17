8b94925e273f13493800e2903c50a924964f1285
Benjamin Bergstrom <benko.dubovecky@gmail.com>
2019-01-17 22:05:34 +0100

    ADD annotations to the API

--- api/app/File.php
+++ api/app/File.php
@@ -21,6 +21,11 @@ class File extends Level {
     /** @var integer */
     protected $capacity;
 
+    /**
+     * @param $capacity
+     * File constructor. Accepts the capacity (i.e. the maximum number of levels for a given levelset)
+     * as a parameter or if it is empty, it defaults to classic value.
+     */
     public function __construct(int $capacity = null) {
         parent::__construct();
 
@@ -31,24 +36,50 @@ class File extends Level {
         }
     }
 
-    public function setFileName($filename) {
+    /**
+     * @param $fileName
+     * @return App\File
+     * Sets the attribute $filename to the value provided in the paramater.
+     * Can have other methods appended to it.
+     */
+    public function setFileName(string $filename) {
         $this->filename = $filename;
         return $this;
     }
 
+    /**
+     * @return string
+     * Gets the value of the attribute $fileName.
+     */
     public function getFileName() {
         return $this->filename;
     }
 
-    public function setType($type) {
+    /**
+     * @param $type
+     * @return App\File
+     * Sets the type of the file that is to be worked with.
+     * Can have other methods appended to it.
+     */
+    public function setType(string $type) {
         $this->type = $type;
         return $this;
     }
 
+    /**
+     * @return string
+     * Gets the value of the atrribute $type.
+     */
     public function getType() {
         return $this->type;
     }
 
+    /**
+     * @param $fileName, $type
+     * @return App\File;
+     * Based on other provided attributes or parameters, this method sets the full
+     * name of the levelset file based on its properties.
+     */
     public function setSource(string $filename = null, string $type = null) {
         $source = '../storage/app/levels/';
         if ($filename) {
@@ -69,17 +100,31 @@ class File extends Level {
         return $this;
     }
 
+    /**
+     * @return string
+     * Gets the value of the attribute $source.
+     */
     public function getSource() {
         return $this->source;
     }
 
-    public function config($fileName, $type) {
+    /**
+     * @param $fileName, $type
+     * @return App\File
+     * Helper method that facilitates lengthy calls and better defines
+     * the nomenclature.
+     */
+    public function config(string $fileName, string $type) {
         $this->setFileName($fileName);
         $this->setType($type);
         $this->setSource($fileName, $type);
         return $this;
     }
 
+    /**
+     * @return App\Level
+     * Returns the entire binary file structure in an array.
+     */
     public function read() {
         $binary = bin2hex(file_get_contents($this->getSource()));
         $binary = str_split($binary, 2);
@@ -109,6 +154,9 @@ class File extends Level {
         return $levels;
     }
 
+    /**
+     * Gets the value of the attribute $capacity.
+     */
     public function getCapacity() {
         return $this->capacity;
     }
--- api/app/Level.php
+++ api/app/Level.php
@@ -21,6 +21,10 @@ class Level {
     /** @var array */
     private $info;
 
+    /**
+     * @param $width, $height
+     * Level constructor that accepts level dimensions in its parameters.
+     */
     public function __construct(int $width = null, int $height = null) {
         if ($width) {
             $this->width = $width;
@@ -37,14 +41,27 @@ class Level {
         $this->grid = $this->width * $this->height;
     }
 
+    /**
+     * @param $id
+     * @return void
+     * Sets an attribute of $levelBinary to the binary clusters representing the level. Can
+     * have other methods appended to it.
+     */
     protected function select(int $id) {
         if (!($id > 0 && $id <= $this->capacity)) throw new \Exception("Number out of range: '$id' is not within the range of 1-{$this->capacity}");
 
         $this->id = $id;
         $start = ($this->id - 1) * $this->grid + 96 * ($this->id - 1);
         $this->levelBinary = array_slice($this->fileBinary, $start, $this->grid);
+        
+        return $this;
     }
 
+    /**
+     * @return App\Level
+     * Sets an atrribute of $info to the binary clusters representing the information
+     * (e.g. needed infotrons, name etc.) about the level. Can have other methods append to it.
+     */
     public function info() {
         $start = $this->id * $this->grid + ($this->id - 1) * 96;
         $this->info = array_slice($this->fileBinary, $start, 96);
@@ -59,8 +76,8 @@ class Level {
     protected function countElements(string $element) {
         $number = 0;
 
-        foreach ($this->levelBinary as $sector) {
-            $name = elementsImages()[$sector];
+        foreach ($this->levelBinary as $cluster) {
+            $name = elementsImages()[$cluster];
 
             if ($element == strtolower($name)) {
                 ++$number;
@@ -70,22 +87,39 @@ class Level {
         return $number;
     }
 
+    /**
+     * @return array
+     * Returns the array of binary cluster forming the level.
+     */
     public function render() {
         return $this->levelBinary;
     }
 
+    /**
+     * @return bool
+     * Gets the gravity state from binary clusters.
+     */
     public function gravity() {
         $this->setInfo();
 
         return !($this->info[4] == 0);
     }
 
+    /**
+     * @return bool
+     * Gets the freeze zonks state from binary clusters.
+     */
     public function freezeZonks() {
         $this->setInfo();
 
         return ($this->info[29] == 2);
     }
 
+    /**
+     * @return int
+     * Gets the number of infotrons based on the requirement, if it is set to nothing, then just
+     * gets the number of all infotrons within the level.
+     */
     public function infotrons() {
         $this->setInfo();
         $cluster = hexdec($this->info[30]);
@@ -93,18 +127,26 @@ class Level {
         return $cluster == 0 ? $this->allInfotrons() : $cluster;
     }
 
+    /**
+     * @return int
+     * Gets the number of all infotrons available within the level.
+     */
     public function allInfotrons() {
         return $this->countElements('infotron');
     }
 
+    /**
+     * @return string
+     * Gets level name as is stated in the binary clusters.
+     */
     public function classicName() {
         $this->setInfo();
 
-        // First we retrieve the data sectors for the level name
+        // First we retrieve the data clusters for the level name
         $name = array_slice($this->info, 6, 23);
 
-        // Then we translate each binary sector into character
-        foreach ($name as $key => $sector) {
+        // Then we translate each binary cluster into character
+        foreach ($name as $key => $cluster) {
             $name[$key] = pack('H*', $name[$key]);
         }
 
@@ -112,6 +154,10 @@ class Level {
         return join('', $name);
     }
 
+    /**
+     * @return string
+     * Gets level name from binary clusters and formats the level name to a more readable form.
+     */
     public function name() {
         $this->setInfo();
 
@@ -141,16 +187,25 @@ class Level {
     }
 
     /**
+     * @return void
      * Sets the parameter of the info attribute if it has been not set yet.
      */
     private function setInfo() {
         if (!$this->info) $this->info();
     }
 
+    /**
+     * @return int
+     * Gets width dimension of the level canvas. 
+     */
     public function getWidth() {
         return $this->width;
     }
 
+    /**
+     * @return int
+     * Gets height dimension of the level canvas.
+     */
     public function getHeight() {
         return $this->height;
     }
