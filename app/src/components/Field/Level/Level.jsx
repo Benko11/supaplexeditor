51b132e4588cd7429699de11d7e3d45dcd575cb2
Benjamin Bergstrom <benko.bergstrom@icloud.com>
2019-06-17 20:34:10 +0200

    ADD Level component

---
+++ app/src/components/Field/Level/Level.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Level extends Component {
+  render() {
+    return (
+      <table className="field">
+        <tr>
+          <td className="base" />
+        </tr>
+      </table>
+    );
+  }
+}
+
+export default Level;
