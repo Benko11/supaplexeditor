d808a5b3ca011085b22595eec4abc52eae538566
Benjamin Bergstrom <benko.bergstrom@icloud.com>
2019-06-17 17:41:07 +0200

    ADD testing component

---
+++ app/src/components/Index.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+const Index = () => {
+  return (
+    <div className="container">
+      <div className="card">
+        <div className="card-body">
+          Hello, guys
+          <div className="btn btn-primary">Hello</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Index;
