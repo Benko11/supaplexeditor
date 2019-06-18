f52f2faaae4c287daaed5fc696df05a7acad5ce9
Benjamin Bergstrom <benko.bergstrom@icloud.com>
2019-06-18 14:48:26 +0200

    ADD status bar

--- app/src/components/Field/Menu/Menu.jsx
+++ app/src/components/Field/Menu/Menu.jsx
@@ -1,19 +1,24 @@
 import React, { Component } from "react";
 import MenuItem from "./MenuItem";
+import Statusbar from "./Statusbar";
 
 class Menu extends Component {
   render() {
     return (
-      <div className="menu">
-        <div className="row">
-          <MenuItem image="upport-transparent" />
-          <MenuItem image="terminal" />
-          <MenuItem image="infotron-transparent" />
-          <MenuItem image="bug" />
-          <MenuItem image="electron-transparent" />
-          <MenuItem image="reddisk-transparent" />
+      <React.Fragment>
+        <div className="menu">
+          <div className="row">
+            <MenuItem image="upport-transparent" />
+            <MenuItem image="terminal" />
+            <MenuItem image="infotron-transparent" />
+            <MenuItem image="bug" />
+            <MenuItem image="electron-transparent" />
+            <MenuItem image="reddisk-transparent" />
+          </div>
         </div>
-      </div>
+
+        <Statusbar />
+      </React.Fragment>
     );
   }
 }
---
+++ app/src/components/Field/Menu/Statusbar.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Statusbar extends Component {
+  render() {
+    return <div className="statusbar" />;
+  }
+}
+
+export default Statusbar;
