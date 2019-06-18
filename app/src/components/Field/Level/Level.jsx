eea5f93a3fa33c25d0c4a7cce5f2418f97c9f093
Benjamin Bergstrom <benko.bergstrom@icloud.com>
2019-06-18 15:21:26 +0200

    ADD api call to Level component

--- app/src/components/Field/Level/Level.jsx
+++ app/src/components/Field/Level/Level.jsx
@@ -1,12 +1,36 @@
 import React, { Component } from "react";
+const axios = require("axios");
 
 class Level extends Component {
+  state = {
+    data: {
+      id: null,
+      capacity: null,
+      width: null,
+      height: null,
+      name: null,
+      infotrons: null,
+      infotronsAll: null,
+      gravity: null,
+      freezeZonks: null,
+      level: []
+    }
+  };
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:3003/api/v1/levels/12")
+      .then(data => console.log(data));
+  }
+
   render() {
     return (
       <table className="field">
-        <tr>
-          <td className="base" />
-        </tr>
+        <tbody>
+          <tr>
+            <td className="base" />
+          </tr>
+        </tbody>
       </table>
     );
   }
